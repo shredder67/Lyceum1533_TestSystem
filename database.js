fd666849da24fe7436233653f56986c078ca3092
shredder67 <31409973+shredder67@users.noreply.github.com>
2018-05-16 22:54:15 +0300

    комментики

--- database.js
+++ database.js
@@ -6,10 +6,10 @@ module.exports = () =>{
         mongoose.Promise = global.Promise;
         mongoose.set('debug', true);
 
-        mongoose.connection
+        mongoose.connection //обработчик соединения
             .on('error', () => reject(error))
             .on('close', () => console.log("connection with db was closed!"))
-            .once('open', () => resolve(mongoose.connections[0]));
+            .once('open', () => resolve(mongoose.connections[0]));//передает объект с информацией о подключенной бд
 
         mongoose.connect(config.MONGO_URL, {useMongoClient: true});
     });
