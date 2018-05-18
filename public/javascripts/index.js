9a5cab9572f5013c6e43b727d7ab6af90105fafe
shredder67 <31409973+shredder67@users.noreply.github.com>
2018-05-18 06:58:51 +0300

    *закоментированна неработающаю кнопка

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,13 +1,13 @@
-function addQuestionButtonClick(){
+/*function addQuestionButtonClick(){
     var selection = document.createElement('form');
     selection.method="get";
     selection.innerText="Выберите тип вопроса";
     var choice1 = document.createElement('input');
     choice1.type="radio";
     choice1.innerText="Свободный ввод"
-    var choice2 = document.createElement('input');
+    var choice2 = document.create<!-- --> Element('input');
     choice1.type="radio";
     choice1.innerText="Выбор правильного из списка"
     selection.appendChild(choice1); selection.appendChild(choice2);
     document.getElementById("post_form").appendChild(selection);
-}
+}*/
