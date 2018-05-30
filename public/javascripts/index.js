401a105a406dc00b50d6d0286a85020387258e88
PokerZloy <37349348+PokerZloy@users.noreply.github.com>
2018-05-30 11:34:07 +0300

    Добавление вопросов

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,13 +1,35 @@
-/*function addQuestionButtonClick(){
-    var selection = document.createElement('form');
-    selection.method="get";
-    selection.innerText="Выберите тип вопроса";
-    var choice1 = document.createElement('input');
-    choice1.type="radio";
-    choice1.innerText="Свободный ввод"
-    var choice2 = document.create<!-- --> Element('input');
-    choice1.type="radio";
-    choice1.innerText="Выбор правильного из списка"
-    selection.appendChild(choice1); selection.appendChild(choice2);
-    document.getElementById("post_form").appendChild(selection);
-}*/
+function addQuestionButtonClick(){
+    
+    document.getElementById("shit").innerHTML='<p><select id="selected_question" onchange="change_type();"><option value="ничего"></option><option value="Вопрос с выбором">Вопрос с выбором</option><option value="Вопрос">Вопрос</option></select></p>';
+    document.getElementById("shit").appendChild(elem);
+}
+function change_type()
+{
+switch(document.getElementById("selected_question").value)
+{
+case 'Вопрос':
+document.getElementById("for_questions").innerHTML='<input type="text">';
+break;
+case 'Вопрос с выбором':
+document.getElementById("for_questions").innerHTML='<input type="text" id="ques_count" onblur="create_questions();">';
+
+//document.getElementById("for_questions").innerHTML='<input name="test1" type="radio" value="nedzen"> Сука <input name="test2" type="radio" value="nedzen"> Убью <input name="test3" type="radio" value="nedzen"> Себя';
+break;
+}
+}
+function create_questions()
+{
+var arr = [];
+for(var i=0;i<document.getElementById("ques_count").value;i++)
+{
+    document.getElementById("for_questions").innerHTML='<input type="text", onblur"">';
+    arr[i]= document.getElementById("for_questions").value;
+}
+
+}
+function add_question()
+{
+
+}
+
+   
