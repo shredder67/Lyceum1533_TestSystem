304313ff1288209486a887eede9b7fb7a4ecc673
PokerZloy <37349348+PokerZloy@users.noreply.github.com>
2018-06-01 10:43:16 +0300

    Я устал чистить шлак

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,42 +1,33 @@
-var index = 0;//индексация id вопроса
+var index =0;//индексация id вопроса
 function addQuestionButtonClick(){
     var elem = document.createElement("div");
-    elem.id = "question" + index;
+    elem.id ="question"+index;
     //выбор типа вопроса 
     elem.innerHTML = `<h6>Выберите тип вопроса</h6>`+
-    `<select name = 'questions[][quesiton_type]' onchange = 'changeQuestionType("${elem.id}", this.options[this.selectedIndex].value)'>`+
+    `<select name = 'questions[][quesiton_type]' onchange = "changeQuestionType('${elem.id}', this.options[this.selectedIndex].value);">`+
     `<option value=''></option><option value = 'free_input'>Свободный ответ</option>`+
-    `<option value = 'multiple_choice'>Выбор из списка</option> </select> <div id = "q_b"></div>`
+    `<option value = 'multiple_choice'>Выбор из списка</option> </select> <div id = "q_b"></div>`;
     document.getElementById("question_list").appendChild(elem)    
     index++;
 }
 
-<<<<<<< HEAD
-function addQuestionButtonClick()
-{
-    switch(document.getElementById("selected_question").value)
-    {
-    case 'Вопрос':
-    document.getElementById("default").innerHTML='';
-    break;
-    case 'Вопрос с выбором':
-            document.getElementById("choice").innerHTML='';
-    
-    break;
-=======
+
+
 function changeQuestionType(question_id, question_type){//смена тела вопроса
-    elem_body = document.getElementById(question_id).getElementsTagName("div");
+     var elem_body = document.getElementById(question_id).getElementsTagName("div");
     switch(question_type){
-        case "free_input": {
-            elem_body.innerHTML = "<h7>Условие:</h7><input type = 'text' name = 'question[][condition]'></input><h7>Правильный ответ:</h7><input type = 'text' name = 'question[][condition]'></input>"
+        case 'free_input': {
+            elem_body.innerHTML = '<h7>Условие:</h7><input type = "text" name = "question[][condition]"></input><h7>Правильный ответ:</h7><input type = "text" name = "question[][condition]"></input>';
+            break;
         }
-        case "multiple_choice":{
-            elem_body.innerHTML = ""
+        case 'multiple_choice':{
+            elem_body.innerHTML = "";
+            break;
         }
         default:{
-            elem_body.innerHTML = "<h4>Выберите тип вопроса!</h4>"
+            elem_body.innerHTML = "<h4>Выберите тип вопроса!</h4>";
+            break;
         }
->>>>>>> d477182471fae008ddde9f978a409ecbd52579ab
     }
 }
 
@@ -62,11 +53,6 @@ function change_type() {
             document.getElementById("choice").innerHTML = '<p>Введите условие</p> <input type="text" name="situation" id="condition"> <p>Введите верный вариант</p><input type="text" id="f" name="right_option"> <p>Введите второй вариант</p><input type="text" id="s" name="all_options[]"><p>Введите третий вариант</p> <input type="text" id="t"name="all_options[]">';
 
 
-<<<<<<< HEAD
-   
-
-=======
             break;
     }
 }*/
->>>>>>> d477182471fae008ddde9f978a409ecbd52579ab
