3a1457dbf95345afa2d1a2f21d7409753e9ba1a3
shredder67 <31409973+shredder67@users.noreply.github.com>
2018-06-03 18:06:22 +0300

    *

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+app.use(logger('dev'));//логгирование в консоль
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
