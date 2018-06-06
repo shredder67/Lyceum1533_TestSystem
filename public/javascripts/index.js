7dc8c7ee71c629d763e27bb56cb3895d0cd0dcb8
takemysoul <39089587+takemysoul@users.noreply.github.com>
2018-06-06 13:48:41 +0300

    дизайн

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,7 @@
 var index = 0;
 var kostil = 0; //индексация id вопроса 
 var fl;
+
 function addQuestionButtonClick() {
     kostil = 0;
     var elem = document.createElement("div");
@@ -15,7 +16,7 @@ function addQuestionButtonClick() {
 }
 
 function changeQuestionType(question_id, question_type) { //смена тела вопроса 
-    var elem_body = document.getElementById(question_id).getElementsByTagName("div")[0]; //elem_body - div внутри одного вопроса, содержащий его тело (условие, варианты ответа итд)
+    var elem_body = document.getElementById(question_id).getElementsByTagName("div")[0]; //elem_body - div внутри одного вопроса, содержащий его тело (условие, варианты ответа итд) 
     switch (question_type) {
         case "free_input":
             {
@@ -24,10 +25,10 @@ function changeQuestionType(question_id, question_type) { //смена тела 
             }
         case "multiple_choice":
             {
-                elem_body.innerHTML = `<h7>Кол-во вариантов ответа:</h7><select id='answer_count' onchange='createNewAnswers(this.options[this.selectedIndex].value,"${elem_body.id}", "${question_id}");'>" +
-"<option value = ''></option>" +
-"<option value = '2'>2</option>" +
-"<option value = '3'>3</option>" +
+                elem_body.innerHTML = `<h7>Кол-во вариантов ответа:</h7><select id='answer_count' onchange='createNewAnswers(this.options[this.selectedIndex].value,"${elem_body.id}", "${question_id}");'>" + 
+"<option value = ''></option>" + 
+"<option value = '2'>2</option>" + 
+"<option value = '3'>3</option>" + 
 "<option value = '4'>4</option></select>`
 
                 break;
@@ -39,7 +40,7 @@ function changeQuestionType(question_id, question_type) { //смена тела 
     }
 }
 
-function createNewAnswers(question_count, elem_body_id, question_id) //Добавление вариантов ответа в multitple_choice
+function createNewAnswers(question_count, elem_body_id, question_id) //Добавление вариантов ответа в multitple_choice 
 {
     if (kostil != 0) {
         document.getElementById("tr[" + index + "]").innerHTML = '';
@@ -47,9 +48,9 @@ function createNewAnswers(question_count, elem_body_id, question_id) //Доба�
     var fix = document.createElement("div");
     fix.id = "tr[" + index + "]";
     document.getElementById(elem_body_id).appendChild(fix);
-    fl =document.createElement("input");
-    fl.type="file";
-    fl.name=`${question_id}[pic]`;
+    fl = document.createElement("input");
+    fl.type = "file";
+    fl.name = `${question_id}[pic]`;
     document.getElementById("tr[" + index + "]").appendChild(fl);
     var rd;
     var options = document.createElement("input");
