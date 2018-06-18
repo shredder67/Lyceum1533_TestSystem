512d6fc52ce74f9ddc8a718e9d042b9a2d4a0728
shredder67 <31409973+shredder67@users.noreply.github.com>
2018-06-18 09:38:21 +0300

    небольшие поправки

--- routes/test_view.js
+++ routes/test_view.js
@@ -2,7 +2,7 @@ var express = require('express');
 var Post = require('../models/test');
 var router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', roleHandler(), (req, res) => {
     if (req.query.test_id.match(/^[0-9a-fA-F]{24}$/)) { //проверка на соотвествие id теста формату
         Post.findById(req.query.test_id, function (err, obj) {
             if (err) {
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
                 res.send('Ошибка. Данный тест отсуствует');
             }
             console.log(obj);
-            res.render('./test_view.ejs', {
+            res.render('./teacher/test_view.ejs', {
                 test: obj,
                 role: req.role
             })
@@ -22,5 +22,18 @@ router.get('/', (req, res) => {
         res.send('Ошибка. Некорректный запрос');
     }
 });
+
+function roleHandler() {
+    return (req,res,next)=>{
+        switch (req.role) {
+            case 'teacher':
+                next();
+                break;
+            case 'student':
+                res.send('КУДА ЭТО ТЫ СОБРАЛСЯ? А НУ ВЕРНИСЬ!');
+                break;
+        }
+    }
+}
 
 module.exports = router;
